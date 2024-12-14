54868dbbe8c2463c77d7b8e3b2d0e7e1dc9d3a3e
SmknSe <semeykin2003@mail.ru>
2024-12-15 00:56:18 +0300

    init

---
+++ src/test/java/com/example/servicedicovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicedicovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
